79b96a0bce4bf16f09d385bf16aa6885be1f224b
LucasK <lucas.kuntz@afpa-dev-pompey.fr>
2025-02-27 09:05:04 +0100

    refonte typescript

--- src/components/header/header.tsx
+++
@@ -1,9 +0,0 @@
-import Navbar from "./navbar/navbar";
-
-export default function Header() {
-    return (
-        <header className={"header"} aria-label={"Header"}>
-          <Navbar />
-        </header>
-    )
-}
