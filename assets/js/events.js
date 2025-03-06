7016baeae9cd5d8b23056aed6c9b19dc1c6cf307
LucasK <lucas.kuntz@afpa-dev-pompey.fr>
2025-03-06 10:41:41 +0100

    Meteo + reformat code

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,57 +1,74 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     /**
      * Sécurité pour les champs chiffre d'affaires et nb employés.
      */
-    if(["Front/clients/create.html", "Front/clients/update.html"].includes(getCurrentPage())){
-        document.querySelector("main form button").addEventListener("click", function(){
-            if(securiteChiffreAffaires()){
-                console.error("Chiffre d'affaires doit être au dessus de" +
-                    " 250€ !");
+    if (["Front/clients/create.html", "Front/clients/update.html"].includes(getCurrentPage())) {
+        document.querySelector("main form button").addEventListener("click", function (e) {
+
+            let msg = "";
+            e.preventDefault();
+
+            if (securiteChiffreAffaires()) {
+                msg += "Chiffre d'affaires doit être au dessus de 250€ !\n";
             }
 
-            if(securiteNbEmployes()){
-                console.error("La société doit compter au moins 1 employé !")
+            if (securiteNbEmployes()) {
+                msg += "La société doit compter au moins 1 employé !\n";
             }
+
+            if (msg !== "") {
+                alert(msg);
+                console.error(msg);
+            }
+
         });
     }
 
     /**
      * Recherche dans le tableau Hovertable
      */
-    if(detectHovertable()){
+    if (detectHovertable()) {
         Array.from(document.querySelectorAll(".hovertable input")).forEach((input) => {
             let row = input.parentElement.parentElement;
-            input.addEventListener("input", function(){
+            input.addEventListener("input", function () {
                 searchValueHovertable(input.value, Array.from(row.children).indexOf(input.parentElement));
             });
         });
         Array.from(document.querySelectorAll(".hovertable select")).forEach((select) => {
             let row = select.parentElement.parentElement;
-            select.addEventListener("change", function(){
+            select.addEventListener("change", function () {
                 searchValueHovertable(select.selectedOptions[0].innerText, Array.from(row.children).indexOf(select.parentElement));
             });
         });
     }
 
-    if(["Front/clients/view.html", "Front/prospects/view.html"]){
-        let map;
-        const adresse = document.getElementById("numeroRueInput").value + " " +
-            document.getElementById("nomRueInput").value + " " +
-            document.getElementById("codePostalInput").value + " " +
-            document.getElementById("villeInput").value;
-
+    if (["Front/clients/view.html", "Front/prospects/view.html"].includes(getCurrentPage())) {
         searchGeolocalisation()
             .then(array => {
-                map = L.map('map').setView(array, 16);
+                createLeafletMap(array);
+                createMeteoModale(array).then(r => {
+                    const t = getTemperature(r);
+                    document.getElementById("resultT").innerText = t[0] + "°C - " + t[1];
 
-                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    maxZoom: 23,
-                    attribution: '&copy; <a href="https://osm.org/copyright%22%3EOpenStreetMap</a> contributors',
-                    id: 'mapbox.streets'
-                }).addTo(map);
+                    const p = getPluie(r);
+                    document.getElementById("resultP").innerText = p[0] + "mm" +
+                        " - " + p[1];
 
-                var marker = L.marker(array).addTo(map);
-                marker.bindPopup(document.getElementById("raisonSocialeInput").value+"<br/>"+adresse).openPopup();
+                    const v = getVent(r);
+                    document.getElementById("resultV").innerText = v[0] + "km/h - " + v[1];
+
+                    const n = getNebulosite(r);
+                    document.getElementById("resultN").innerText = n[0] + "% -" +
+                        " " + n[1];
+
+                    const h = getHumidite(r);
+                    document.getElementById("resultH").innerText = h[0] + "% -" +
+                        " " + h[1];
+
+                    const labelMeteo = getTypeMeteo(t[0], p[0], v[0], n[0], h[0]);
+                    document.getElementById("modalMeteo").innerText = labelMeteo;
+                    document.getElementById("labelAdresseMeteo").innerText = labelMeteo;
+                });
             });
     }
 });
--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -1,42 +1,42 @@
-function getCurrentPage(){
+function getCurrentPage() {
     const pathArray = window.location.pathname.split("/");
     let page;
 
-    if(pathArray.length === 9){
+    if (pathArray.length === 9) {
         page = pathArray[7] + "/" + pathArray[8];
-    }else if(pathArray.length === 10){
+    } else if (pathArray.length === 10) {
         page = pathArray[7] + "/" + pathArray[8] + "/" + pathArray[9];
     }
     return page;
 }
 
-function securiteChiffreAffaires(){
+function securiteChiffreAffaires() {
     return document.querySelector("form input#chiffreAffairesInput").value < 200;
 }
 
-function securiteNbEmployes(){
+function securiteNbEmployes() {
     return document.querySelector("form input#nbEmployesInput").value < 1;
 }
 
-function detectHovertable(){
+function detectHovertable() {
     return document.querySelector(".hovertable") !== null;
 }
 
-function searchValueHovertable(value, column){
+function searchValueHovertable(value, column) {
     Array.from(document.querySelectorAll(".hovertable .hovertable-body .hovertable-row")).forEach((row) => {
         let searchFalse;
 
-        if(row.getAttribute("searchFalse") !== null){
+        if (row.getAttribute("searchFalse") !== null) {
             searchFalse = JSON.parse(row.getAttribute("searchFalse"));
-        }else{
+        } else {
             searchFalse = [];
         }
 
-        if(!row.children[column].innerText.includes(value) && value !== ""){
+        if (!row.children[column].innerText.includes(value) && value !== "") {
             searchFalse.push(column);
-        }else{
+        } else {
             let index = searchFalse.indexOf(column);
-            if(index > -1){
+            if (index > -1) {
                 searchFalse.splice(index, 1);
             }
         }
@@ -46,18 +46,18 @@ function searchValueHovertable(value, column){
     hideRowIfMarked();
 }
 
-function hideRowIfMarked(){
+function hideRowIfMarked() {
     Array.from(document.querySelectorAll(".hovertable .hovertable-body .hovertable-row")).forEach((row) => {
-        console.log(row.children[0].innerHTML.trim() + " : "+row.getAttribute("searchFalse"));
+        console.log(row.children[0].innerHTML.trim() + " : " + row.getAttribute("searchFalse"));
         let searchFalse;
 
-        if(row.getAttribute("searchFalse") !== null){
+        if (row.getAttribute("searchFalse") !== null) {
             searchFalse = JSON.parse(row.getAttribute("searchFalse"));
         }
 
-        if(searchFalse.length > 0){
+        if (searchFalse.length > 0) {
             row.setAttribute("hidden", "hidden");
-        }else{
+        } else {
             row.removeAttribute("hidden");
         }
     });
@@ -78,4 +78,191 @@ async function searchGeolocalisation() {
     const json = await response.json();
 
     return [json.features[0].geometry.coordinates[1], json.features[0].geometry.coordinates[0]];
+}
+
+function createLeafletMap(array) {
+    const adresse = document.getElementById("numeroRueInput").value + " " +
+        document.getElementById("nomRueInput").value + " " +
+        document.getElementById("codePostalInput").value + " " +
+        document.getElementById("villeInput").value;
+
+    map = L.map('map').setView(array, 16);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 23,
+        attribution: '&copy; <a href="https://osm.org/copyright%22%3EOpenStreetMap</a> contributors',
+        id: 'mapbox.streets'
+    }).addTo(map);
+
+    var marker = L.marker(array).addTo(map);
+    marker.bindPopup(document.getElementById("raisonSocialeInput").value + "<br/>" + adresse).openPopup();
+}
+
+async function createMeteoModale(array) {
+    const url = "https://www.infoclimat.fr/public-api/gfs/json?_ll=" + array[0] + "," + array[1] +
+        "&_auth=Bx0DFAV7U3FRfFZhUCYFLAdvAjcLfQUiAX1QM1w5XyJWPVAxBGRQNgRqWicAL1dhUH0EZww3CTkHbAJ6WigDYgdtA28FblM0UT5WM1B%2FBS4HPQJnCzwFNAFrUChcLl89Vj1QPAR5UDAEaFo5AC5XZVBmBHoMMgkxB2wCelooA2EHYwNjBWdTMVE3VjNQZAUxBzICfQsrBTsBZVBkXDZfPFY8UGYEMlBhBGlaPAA4VzBQZAR6DDEJMQdgAmNaMANjB2ADYwV5Uy5RR1ZHUH0FcQd2AjcLcgUgATdQaVxl&_c=83c427140f87bd975fcfb36035f83e4b";
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+
+    try {
+        const date = new Date();
+        let dateString = date.toISOString().split("T")[0];
+
+        if (date.getHours() <= 1) {
+            dateString += " 01:00:00";
+        } else if (date.getHours() > 1 && date.getHours() <= 4) {
+            dateString += " 04:00:00";
+        } else if (date.getHours() > 4 && date.getHours() <= 7) {
+            dateString += " 07:00:00";
+        } else if (date.getHours() > 7 && date.getHours() <= 10) {
+            dateString += " 10:00:00";
+        } else if (date.getHours() > 10 && date.getHours() <= 13) {
+            dateString += " 13:00:00";
+        } else if (date.getHours() > 13 && date.getHours() <= 16) {
+            dateString += " 16:00:00";
+        } else if (date.getHours() > 16 && date.getHours() <= 19) {
+            dateString += " 19:00:00";
+        } else if (date.getHours() > 19 && date.getHours() <= 22) {
+            dateString += " 22:00:00";
+        } else if (date.getHours() > 22) {
+            date.setDate(date.getDate() + 1);
+            dateString = date.toISOString().split("T")[0] + "01:00:00";
+        }
+
+        return json[dateString];
+    } catch (error) {
+        console.error(error);
+        return "";
+    }
+}
+
+function getTemperature(json) {
+    const valeur = Math.floor(json.temperature["sol"] - 273.15);
+
+    const libelle = (valeur < 0 && "Très froid") ||
+        (valeur >= 0 && valeur < 10 && "Froid") ||
+        (valeur >= 10 && valeur < 20 && "Frais") ||
+        (valeur >= 20 && valeur < 30 && "Doux") ||
+        (valeur >= 30 && "Chaud/Canicule");
+
+    return [valeur, libelle];
+}
+
+function getPluie(json) {
+    const valeur = Math.floor(json.pluie);
+
+    const libelle = (valeur === 0 && "Aucune") ||
+        (valeur > 0 && valeur < 2 && "Très faible") ||
+        (valeur >= 2 && valeur < 10 && "Faible") ||
+        (valeur >= 10 && valeur < 30 && "Modérée") ||
+        (valeur >= 30 && valeur < 50 && "Forte") ||
+        (valeur >= 50 && "Très forte");
+
+    return [valeur, libelle];
+}
+
+function getVent(json) {
+    const valeur = Math.floor(json.vent_moyen["10m"]);
+
+    const libelle = (valeur >= 0 && valeur < 10 && "Calme") ||
+        (valeur >= 10 && valeur < 20 && "Léger") ||
+        (valeur >= 20 && valeur < 40 && "Modéré") ||
+        (valeur >= 40 && valeur < 60 && "Fort") ||
+        (valeur >= 60 && "Tempête/Ouragan");
+
+    return [valeur, libelle];
+}
+
+function getNebulosite(json) {
+    const valeur = Math.floor(json.nebulosite["totale"]);
+
+    const libelle = (valeur >= 0 && valeur < 20 && "Dégagé") ||
+        (valeur >= 20 && valeur < 50 && "Partiellement nuageux") ||
+        (valeur >= 50 && valeur < 80 && "Nuageux") ||
+        (valeur >= 80 && valeur < 100 && "Très nuageux") ||
+        (valeur >= 100 && "Brouillard");
+
+    return [valeur, libelle];
+}
+
+function getHumidite(json) {
+    const valeur = Math.floor(json.humidite["2m"]);
+
+    const libelle = (valeur < 30 && "Très sec") ||
+        (valeur >= 30 && valeur < 50 && "Sec") ||
+        (valeur >= 50 && valeur < 70 && "Modéré") ||
+        (valeur >= 70 && valeur < 90 && "Humide") ||
+        (valeur >= 90 && "Très humide");
+
+    return [valeur, libelle];
+}
+
+function getTypeMeteo(valueTemperature, valuePluie, valueVent, valueNebulosite, valueHumidite) {
+    if (valueTemperature >= 10 && valueTemperature < 30 &&
+        valuePluie >= 0 && valuePluie < 2 &&
+        valueVent >= 0 && valueVent < 20 &&
+        valueNebulosite >= 0 && valueNebulosite < 20 &&
+        valueHumidite >= 30 && valueHumidite < 70) {
+        return "Météo claire et ensoleillée"
+    } else if (valueTemperature >= 10 && valueTemperature < 30 &&
+        valuePluie >= 2 && valuePluie < 30 &&
+        valueVent >= 10 && valueVent < 40 &&
+        valueNebulosite >= 50 && valueNebulosite <= 100 &&
+        valueHumidite >= 50 && valueHumidite < 90) {
+        return "Météo nuageuse/grise"
+    } else if (valueTemperature >= 5 && valueTemperature < 25 &&
+        valuePluie >= 2 && valuePluie < 10 &&
+        valueVent >= 10 && valueVent < 20 &&
+        valueNebulosite >= 20 && valueNebulosite < 80 &&
+        valueHumidite >= 70 && valueHumidite < 90) {
+        return "Pluie légère (Bruine/Averse)"
+    } else if (valueTemperature >= 15 && valueTemperature < 30 &&
+        valuePluie > 30 &&
+        valueVent > 40 &&
+        valueNebulosite >= 80 && valueNebulosite <= 100 &&
+        valueHumidite > 90) {
+        return "Orage/Pluie intense"
+    } else if (valueTemperature > 30 &&
+        valuePluie >= 0 && valuePluie < 2 &&
+        valueVent >= 0 && valueVent < 20 &&
+        valueNebulosite >= 0 && valueNebulosite < 50 &&
+        valueHumidite >= 30 && valueHumidite < 90) {
+        return "Canicule"
+    } else if (valuePluie >= 0 && valuePluie < 30 &&
+        valueVent > 40 &&
+        valueNebulosite >= 50 && valueNebulosite <= 100 &&
+        valueHumidite >= 50 && valueHumidite < 90) {
+        return "Tempête de vent (Vent fort)"
+    } else if (valueTemperature >= 0 && valueTemperature < 15 &&
+        valuePluie >= 0 && valuePluie < 2 &&
+        valueVent >= 0 && valueVent < 10 &&
+        valueNebulosite === 100 &&
+        valueHumidite > 90) {
+        return "Brouillard"
+    } else if (valueTemperature < 0 &&
+        valuePluie >= 10 && valuePluie <= 100 &&
+        valueVent >= 10 && valueVent < 40 &&
+        valueNebulosite >= 50 && valueNebulosite <= 100 &&
+        valueHumidite >= 50 && valueHumidite < 90) {
+        return "Neige"
+    } else if (valueTemperature >= 10 && valueTemperature < 35 &&
+        valuePluie >= 0 && valuePluie < 2 &&
+        valueVent >= 10 && valueVent < 40 &&
+        valueNebulosite >= 0 && valueNebulosite < 20 &&
+        valueHumidite < 30) {
+        return "Temps sec et aride"
+    } else if (valueTemperature > 25 &&
+        valuePluie >= 10 &&
+        valueVent >= 10 && valueVent < 60 &&
+        valueNebulosite >= 20 && valueNebulosite <= 100 &&
+        valueHumidite > 80) {
+        return "Temps tropical (chaud et humide)"
+    } else {
+        return "Météo changeante"
+    }
 }
